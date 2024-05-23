9037a99739561397155736fe9d57969540308d1a
Satyam12m <144460168+Satyam12m@users.noreply.github.com>
2024-05-23 17:10:59 +0530

    Add files via upload

---
+++ AssignmentL1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Q.1
+    cout<<"Physics"<<endl;
+    cout<<"Wallah"<<endl;
+
+    //Q.2
+    int x=5;
+    int y=5;
+    cout<<x+y<<endl;
+
+    //Q.3
+    //How much space does following data types take ?
+   // int 4 bytes
+    //bool 1 byte
+    //float 4 bytes
+
+    //Q.4
+    //Output
+	int a=4;
+    int b=5;
+    a++;b--;
+    cout<<++a<<" "<<b--<<endl;
+    //output will be 6 4
+
+    //Q.5
+    int r=10;
+    cout<<"Area of circle of radius 10 is: "<<3.14*r*r<<endl;
+
+    //Q.6 How many of these can be a variable name?
+    // 01PWskills  no
+    // _FLOAT yes   
+    // int no
+    // FLOAT yes
+    // You will succeed no
+}
+
